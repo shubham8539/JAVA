c946e39ad05884483e20006ca85c6e669344e2ed
Shubham Kumar <kumarshubham853904@gmail.com>
2022-07-12 12:47:20 +0530

    commit

---
+++ araryss.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class araryss {
+    public static void main(String[] args) {
+        
+   Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   int [] arr=new int[n];
+   for(int i=0;i<n;i++){
+    arr[i]=s.nextInt();
+
+   }
+   for(int i=0;i<n;i++){
+
+ System.out.println("YOU ARRAY IS");
+    System.out.print(arr[i]+" ");
+   }
+
+
+
+    }
+
+
+}
